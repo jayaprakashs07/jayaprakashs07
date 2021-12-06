8d055184e3ef7e067dec9a0730900f775d9d7688
Jayaprakash S <jp.jayaprakash779@gmail.com>
2021-12-06 09:31:50 +0530

    Initial commit

---
+++ src/main/java/com/example/jayaprakashs07/Jayaprakashs07Application.java
@@ -0,0 +1,13 @@
+package com.example.jayaprakashs07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jayaprakashs07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jayaprakashs07Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jayaprakashs07/Jayaprakashs07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jayaprakashs07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jayaprakashs07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
